ed1f0790e02af44f755c81975300d760c86e9b3b
Elvira Firmansyah <79557154+elvirafimansyah@users.noreply.github.com>
2021-07-08 18:02:51 +0700

    Portfolio's

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*==================== FORM FUNCITON ====================*/ 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyrnF-v9mgKQBuVQ4KInPE0ECi5YeNTeNyFH45ysVZy-ZLjYKlPHzB83oCiX6rLy2tUrg/exec'
 const form = document.forms['contact-my-website']
 const btnKirim = document.querySelector('.btn-kirim');
@@ -45,3 +46,24 @@ let trans = () => {
         document.documentElement.classList.remove('transition')
     }, 1000)
 }
+
+/*==================== NAVBAR SHADOW ====================*/ 
+// panggil navbar
+const navBar = document.getElementById("navbar");
+const navCont = document.getElementById("navCont");
+function scroll() {
+  let calc = window.scrollY; // mendapatkan posisi scroll dari atas ke bawah
+  if (calc > 0) { // jika posisi scroll lebih dari 0 pixel
+    navBar.classList.replace("bg-transparent", "bg-shadow"); // ganti background navbar
+    navCont.classList.replace("container-fluid", "container"); // ubah margin pada navbar
+  } else if (calc <= 0) { // jika posisi scroll sama dengan 0 pixel
+    navBar.classList.replace("bg-dark", "bg-transparent");
+    navCont.classList.replace("container-fluid", "container");
+  } 
+}
+//panggil saat inisialisasi
+scroll();
+// panggil saat discroll
+window.onscroll = () => { // jika tidak work untuk arrow function, coba pakai function biasa
+  scroll();
+};
